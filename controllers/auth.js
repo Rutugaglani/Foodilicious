09cafb431fdd3b551ae524f286103ad6d5d4a706
rutug <rutugaglani@gmail.com>
2020-11-07 16:14:26 +0530

    address apis added

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,6 +15,7 @@ exports.register = (req,res)=>{
     console.log(req.body);
 
     const {password,email,username}= req.body;
+    
     db.query('SELECT email FROM users WHERE email = ?',[email], async(error,response)=>{
         if(error){
             console.log(error);
@@ -22,7 +23,7 @@ exports.register = (req,res)=>{
         if(response.length > 0)
         {
             console.log("this email is already taken")
-            return res.status(400).json({email : "this email is already taken"});
+            return res.status(201).json({message : "this email is already taken"});
         }
         let hashedPassword = await bcrypt.hash(password,8);
         console.log(hashedPassword);
@@ -32,45 +33,163 @@ exports.register = (req,res)=>{
                 console.log(error)
             }
             else{
-                console.log(res);
-               return res.status(200).json({message: 'User Registered'})
-            }
-        })
+                console.log(response);
+              
+                      
+                db.query('SELECT * from users WHERE email = ?',[email],async(error,results)=>{
+                    try{
+                        if(results.length > 0)
+                        {
+                            const comparision = await bcrypt.compare(
+                                password,
+                                results[0].password
+                              );
+                              if (comparision) {
+                                const id = results[0].id;
+                                const token = jwt.sign({id},process.env.JWT_SECRET,{
+                                    expiresIn: process.env.JWT_EXPIRES_IN
+                                });
+                                
+                
+                                const cookieOpt ={
+                                    expires : new Date(
+                                        Date.now() + process.env.JWT_COOKIE_EXPIRES *24*60*60*1000
+                
+                                    ),
+                                    httpOnly : true
+                                }
+                                res.cookie('jwt',token,cookieOpt);
+                                return res.status(200).json({token:token})
+                              }
+                             else {
+                                res.status(201).send({
+                                  message: "Password is incorrect",
+                                });
+                              }
+                        }
+                        else{
+                            return res.status(201).json({message:"Email does not exist"})
+                        }
+                    }
+                        
+                    catch(error)
+                    {
+                        console.log(error)
+                    }
+                }) 
+              
+            
+        }
+      
+    
     })
     
+})
 }
 exports.login = async (req,res)=>{
     try{
         const {password,email}= req.body;
-        db.query('SELECT * from users WHERE email = ?',[email],async(error,results)=>{
-            if(!results || !(await bcrypt.compare(password,results[0].password)))
-            {
-                res.status(401).json({message:"Email or Password is incorrect"})
-            }
-            else{
-                const id = results[0].id;
-                const token = jwt.sign({id},process.env.JWT_SECRET,{
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-                console.log(token);
-
-                const cookieOpt ={
-                    expires : new Date(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRES *24*60*60*1000
-
-                    ),
-                    httpOnly : true
+        db.query('SELECT * from users WHERE email = ?',[email],async(err,results)=>{
+            try{
+                if(results.length > 0)
+                {
+                    const comparision = await bcrypt.compare(
+                        password,
+                        results[0].password
+                      );
+                      if (comparision) {
+                        const id = results[0].id;
+                        const token = jwt.sign({id},process.env.JWT_SECRET,{
+                            expiresIn: process.env.JWT_EXPIRES_IN
+                        });
+                        
+        
+                        const cookieOpt ={
+                            expires : new Date(
+                                Date.now() + process.env.JWT_COOKIE_EXPIRES *24*60*60*1000
+        
+                            ),
+                            httpOnly : true
+                        }
+                        res.cookie('jwt',token,cookieOpt);
+                        return res.status(200).json({token:token})
+                      }
+                     else {
+                        res.status(201).send({
+                          message: "Password is incorrect",
+                        });
+                      }
                 }
-                res.cookie('jwt',token,cookieOpt);
-                res.status(200).json({token:token})
-
-
+                else{
+                    return res.status(201).json({message:"Email does not exist"})
+                }
+            }
+                
+            catch(err)
+            {
+                console.log(err)
             }
         })
     }
     catch(error)
     {
-console.log(error)
+     console.log(error)
     }
     
 }
+exports.getUser = (req,res) => {
+
+    db.query(`select id,username,email from users where email = ?`,[req.user.email],async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+exports.getCustomerAddress=(req,res)=>{
+    const {id}=req.user;
+    console.log(id)
+    db.query(`select address,city,pincode from customer where user_id = ${id}`,async (err,result) => {
+        try{
+            if(result.length>0)
+            {
+                res.status(200).json(result);
+            }
+            else
+            {
+                res.status(200).json({message:"No Address Registered"});
+            }
+        }
+        catch{
+            console.log(err)
+        }
+
+    })
+}
+exports.updateCustomerAddress=(req,res)=>{
+    const {address,city,pincode,user_id}=req.body;
+ 
+    db.query(`update customer set address="${address}",city="${city}",pincode=${pincode} where user_id = ${user_id}`,async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+exports.insertCustomerAddress=(req,res)=>{
+    const {address,city,pincode,user_id}=req.body;
+ 
+    db.query(`insert into customer(user_id,address,city,pincode) values(${user_id},"${address}","${city}",${pincode}`,async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+
--- controllers/data.js
+++ controllers/data.js
@@ -8,8 +8,10 @@ const db = mysql.createConnection({
   
 });
 
-exports.subCategory = (req,res)=>{
- const id=req.params.id
+
+
+exports.category = (req,res)=>{
+ const id=req.params.id;
     db.query(`SELECT * FROM subcategory WHERE category_id = ${id}`, async(error,response)=>{
         if(error){
             console.log(error);
@@ -22,6 +24,21 @@ exports.subCategory = (req,res)=>{
     })
     
 }
+
+exports.subCategory = (req,res)=>{
+    const id=req.params.id
+       db.query(`SELECT * FROM subcategory WHERE id = ${id}`, async(error,response)=>{
+           if(error){
+               console.log(error);
+           }
+           else
+           {  
+               return res.status(200).json(response);
+           }
+           
+       })
+       
+   }
 exports.restaurantInfo = (req,res)=>{
     const id=req.params.id
     const locId=req.params.locId
@@ -39,7 +56,9 @@ exports.restaurantInfo = (req,res)=>{
    }
    exports.subCatList = (req,res)=>{
     const id=req.params.id
-       db.query(`SELECT Distinct name,rating,type FROM fallunder as FU,restaurant as rest ,restaurantaddress as RA WHERE subcategory_id = ${id} AND FU.restaurant_id=rest.restaurant_id   `, async(error,response)=>{
+       db.query(`SELECT Distinct name,rating,rest.restaurant_id,RA.locality,RA.id,rest.images 
+       FROM fallunder as FU,restaurant as rest ,restaurantaddress as RA
+        WHERE subcategory_id = ${id} AND FU.restaurant_id=rest.restaurant_id AND RA.rest_id=rest.restaurant_id  `, async(error,response)=>{
            if(error){
                console.log(error);
            }
@@ -51,3 +70,309 @@ exports.restaurantInfo = (req,res)=>{
        })
        
    }
+
+   exports.getReview = (req,res) => {
+    const {id} = req.params;
+    db.query(`select * from review where reviewid = ${id}`,async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+
+exports.postReview = (req,res) => {
+    const {review_body,user_id,restaurant_id,rating} = req.body;
+    //console.log(body,user_id,restaurant_id);
+        db.query(`insert into review(review_body,user_id,restaurant_id,rating) values('${review_body}',${user_id},${restaurant_id},${rating})`,async (err,result) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                res.status(200).json(result);
+            }
+        })
+}
+
+exports.getReviewByUser = (req,res) => {
+    const id = req.user.id;
+    db.query(`select users.username,review.date_time,review.review_body,review.id,review.rating,restaurant.name from users join review on users.id=${id} and users.id =review.user_id  join restaurant on restaurant.restaurant_id = review.restaurant_id `,async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+
+
+
+exports.getReviewByRestaurant = (req,res) => {
+    const {id} = req.params;
+    db.query(`select users.username,review.id,review.date_time,review.review_body,review.rating from review join users on users.id = review.user_id AND restaurant_id = ${id}`,async (err,result) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.status(200).json(result);
+        }
+    })
+}
+
+   // Menu
+
+exports.getMenu = (req,res)=>{
+    const {id} = req.params;
+     db.query(`select * from hasmenuof join fooditem on hasmenuof.food_id=fooditem.id and hasmenuof.restaurant_id = ${id}`,async (err,result) => {
+        try{
+            return res.status(200).json(result) ;    
+           
+        }
+        catch{
+            console.log(err);
+        }
+        
+    })
+
+} 
+
+exports.getVegMenu = (req,res)=>{
+    const {id} = req.params;
+     db.query(`select * from hasmenuof join fooditem on hasmenuof.food_id=fooditem.id and fooditem.category='Veg' and hasmenuof.restaurant_id = ${id}`,async (err,result) => {
+        try{
+            return res.status(200).json(result) ;    
+        }
+        catch{
+            console.log(err);
+        }
+        
+    })
+
+} 
+
+//update quantity
+exports.updateQuantity = (req,res) => {
+    const {user_id,food_id,quantity,restaurant_id} = req.body;
+    if(quantity == 0)
+    {
+            db.query(`DELETE FROM cart WHERE user_id=${user_id} AND restaurant_id=${restaurant_id} AND food_id=${food_id}`,async(err,result) => {
+                try{
+                    console.log(quantity,food_id,restaurant_id,user_id)
+                 return res.status(200).json({message : "Item Deleted !!"});
+                }
+         catch
+         {
+             console.log(err)
+        }
+    })
+}
+
+   else{ 
+       db.query(`update cart set quantity=${quantity} ,cost = (select ${quantity}*price from fooditem where id=${food_id}) 
+    where user_id=${user_id} AND restaurant_id=${restaurant_id} AND food_id =${food_id}`,async(err,result) => {
+       try{
+           console.log(quantity,food_id,user_id)
+        return res.status(200).json({message : "values updated"});
+       }
+catch
+{
+    console.log(err)
+}
+           
+    })
+
+
+}
+}
+
+
+//Cart
+exports.cart = (req,res) => {
+    const {user_id,food_id,restaurant_id,quantity} = req.body;
+    db.query(`insert into cart (user_id, food_id, quantity,restaurant_id, cost) values (${user_id},${food_id},${quantity},${restaurant_id},(select ${quantity}*price from fooditem where id=${food_id}))`,async (err,result) => {
+      try{
+        res.status(200).json(result);
+      }
+           
+      catch{
+          console.log(err)
+      }
+    })
+}
+
+exports.getCart = (req,res)=>{
+    const {user_id,restaurant_id} = req.params;
+     db.query(`select * from cart join fooditem on cart.food_id = fooditem.id where user_id=${user_id} AND restaurant_id=${restaurant_id}`,async (err,result) => {
+        try{
+            if(result.length>0)
+            {
+                return res.status(200).json(result) ; 
+            }
+            else
+            {
+                return res.status(200).json({message:"cart is empty"}) ;
+            }     
+        }
+        catch{
+            console.log(err);
+        }
+        
+    })
+
+} 
+
+exports.getCartItem = (req,res) => {
+    const {user_id,food_id,rest_id} = req.params;
+    db.query(`select * from cart where user_id=${user_id} AND restaurant_id=${rest_id} AND food_id=${food_id}`,async (err,result) => {
+      try{
+          if(result.length > 0)
+        res.status(200).json(result);
+        else
+        res.status(200).json({message:"item doesnt exist in cart"});
+      }
+           
+      catch{
+          console.log(err)
+      }
+    })
+}
+exports.getFoodItem = (req,res) => {
+    const {id} = req.params;
+    db.query(`select * from fooditem where  id=${id}`,async (err,result) => {
+      try{
+          if(result.length > 0)
+        res.status(200).json(result);
+        else
+        res.status(200).json({message:"item doesnt exist in cart"});
+      }
+           
+      catch{
+          console.log(err)
+      }
+    })
+}
+
+//Place Order
+exports.placeOrder = (req,res) => {
+    const {restaurant_id,user_id} = req.body;
+    db.query(`insert into orders (cost, restaurant_id, user_id) values((select sum(cost) from cart where user_id=${user_id}), ${restaurant_id}, ${user_id})`,async (err,result) => {
+        if(err) 
+            console.log(err);
+        
+        else
+            res.status(200).json(result);
+    })
+}
+
+exports.updateOrder = (req,res) => {
+    const {restaurant_id,user_id,order_id} = req.body;
+    db.query(`update orders set cost=(select sum(cost) from cart where user_id=${user_id} AND restaurant_id=${restaurant_id} ) where id=${order_id}`,async (err,result) => {
+        if(err) 
+            console.log(err);
+        
+        else
+            res.status(200).json(result);
+    })
+}
+
+//Get Order
+exports.getCurrentOrder = (req,res) => {
+    const {restaurant_id,user_id} = req.params;
+    db.query(`select * from orders where  user_id=${user_id} AND statusOrder=0 AND restaurant_id=${restaurant_id}`,async (err,result) => {
+        if(err) 
+            console.log(err);
+        
+        else
+            res.status(200).json(result);
+    })
+}
+
+//Payment
+exports.payment = (req,res) => {
+    const {payment_method,order_id} = req.body;
+    db.query(`insert into payment (payment_method,order_id) values(${payment_method},${order_id})`,async (err,result) => {
+        try{
+            console.log(payment_method,order_id)
+            db.query(`update orders set statusOrder=1 where id=${order_id}`,async (err,resu) => {
+
+                try{
+                    console.log(order_id)
+                    
+                    return res.status(200).json(resu);
+                }
+                catch{
+                    console.log(err)
+                }
+               
+            });
+            return res.status(200).json(result);
+        }
+        catch{
+            console.log(err)
+        }
+          
+    });
+   
+}
+
+
+exports.deleteCart = (req,res) => {
+    const {user_id,restaurant_id} = req.body;
+            db.query(`DELETE FROM cart WHERE user_id=${user_id} AND restaurant_id=${restaurant_id} `,async(err,result) => {
+                try{
+                    console.log(restaurant_id,user_id)
+                 return res.status(200).json({message : "Item Deleted !!"});
+                }
+         catch
+         {
+             console.log(err)
+        }
+    })
+}
+
+//Search
+exports.search = (req,res)=>{
+    const str=req.body.str
+       db.query(`SELECT Distinct name,rating,rest.restaurant_id,RA.locality,RA.id,rest.images FROM fallunder as FU,restaurant as rest ,restaurantaddress as RA WHERE name LIKE '%${str}%' AND FU.restaurant_id=rest.restaurant_id AND RA.rest_id=rest.restaurant_id 
+    UNION
+    SELECT Distinct name,rating,rest.restaurant_id,RA.locality,RA.id,rest.images FROM fallunder as FU,restaurant as rest ,restaurantaddress as RA WHERE RA.locality LIKE '%${str}%' AND FU.restaurant_id=rest.restaurant_id AND RA.rest_id=rest.restaurant_id 
+
+       `, async(error,response)=>{
+           if(error){
+               console.log(error);
+           }
+           else
+           {  
+               if(response.length >0)
+               {
+                return res.status(200).json(response);
+               }
+               else{
+                return res.status(200).json("No results Found"); 
+               }
+               
+           }
+           
+       })
+       
+   }
+
+ 
+
+
+
+   
+
+
+
+   //by cuisine
+   /*
+       UNION
+    SELECT Distinct rest.name,rest.rating,rest.restaurant_id,RA.locality,RA.id,rest.images 
+    FROM fallunder as FU, restaurant as rest ,restaurantaddress as RA ,subcategory as SC
+    WHERE  RA.rest_id=rest.restaurant_id AND SC.category_id =3 AND SC.name LIKE '%${str}%' AND rest.restaurant_id  = FU.restaurant_id
+    */
+ 
